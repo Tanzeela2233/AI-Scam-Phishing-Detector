56ffde42622c62dcbbf86803a5b450b8727d9ea6
Tanzeela <tanzeelaasgharasghar@gmail.com>
2026-09-16 20:06:55 +0500

    Update app.py

--- app.py
+++ app.py
@@ -434,13 +434,14 @@ with st.sidebar:
         placeholder="Enter your Groq API key"
     )
 
-    model = st.selectbox(
-        "AI Model",
-        [
-            "llama-3.3-70b-versatile",
-            "llama-3.1-8b-instant"
-        ]
-    )
+   model = st.selectbox(
+    "AI Model",
+    [
+        "openai/gpt-oss-20b",
+        "openai/gpt-oss-120b",
+        "qwen/qwen3.6-27b"
+    ]
+)
 
     st.divider()
 
